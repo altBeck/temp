96b282d4e3a08d51442929f79cfdb523d7400030
Kanno Beck <drkannobeck@gmail.com>
2022-07-28 21:28:59 -0700

    Delete layouts directory

--- layouts/app/script.js
+++
@@ -1,9 +0,0 @@
-import LocomotiveScroll from "https://cdn.skypack.dev/locomotive-scroll";
-
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  tablet: { smooth: true },
-  smartphone: { smooth: true }
-});
-
